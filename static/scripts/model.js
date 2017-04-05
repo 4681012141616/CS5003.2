adfe7e7cafd47cb4f6a7e7d57fb1d72e3eb3206b
YiZhang1994 <yizhang@8afbfde6.st-andrews.ac.uk>
2017-04-05 23:33:26 +0100

    remove duplicate

--- static/scripts/model.js
+++ static/scripts/model.js
@@ -43,49 +43,6 @@ class Topic {
     }
 }
 
-class Topic {
-    constructor(topic, userId, content, destinationId, date, replies) {
-        this._id = topic.replace(/\s/g,"_");
-        this._topic = topic;
-        this._userId = userId;
-        this._content = content;
-        this._destinationId = destinationId;
-        this._date = date;
-        this._type = "topic";
-        this._replies = replies;
-    }
-
-    static fromJSON(json) {
-        var topic = json.topic;
-        var userId = json.userId;
-        var content = json.content;
-        var destinationId = json.destinationId;
-        var date = json.date;
-        var replies = json.replies;
-
-
-//TODO: add validation here!!
-        if(!userId) {
-            return null;
-        }
-
-        return new Topic(topic, userId, content, destinationId, date, replies);
-    }
-
-    toJSON() {
-        return {
-            _id:this._id,
-            topic:this._topic,
-            userId:this._userId,
-            content: this._content,
-            destinationId: this._destinationId,
-            date: this._date,
-            type: this._type,
-            replies: this._replies
-        };
-    }
-}
-
 class User {
     constructor(username, password, email, startDate) {
         this._id = "user_"+ username;
