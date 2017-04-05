c0a3ddd952b23fd59f1a3101e46f835d34e33abb
Alastair Stanley <alastairstanley@ntlworld.com>
2017-04-05 12:12:30 +0100

    incl. hash function

--- static/scripts/crypto/test-crypto.js
+++ static/scripts/crypto/test-crypto.js
@@ -4,53 +4,31 @@ $('#checkBtn').click(function(){
 
 
 
-function passwordCheck( userId, password ) {
-
-  return (CryptoJS.SHA256(password).toString(CryptoJS.enc.Base64) == testUser.passwordHash);
-
+function hash( str ) {
+  return CryptoJS.SHA256(hash).toString(CryptoJS.enc.Base64);
 }
 
 
 
-
-// example data only
-
-var testUser = {
-  '_id': 'ajs38',
-  '_rev': 'whatever',
-  
-  // NEW!!!
-  'passwordHash': 'i0DRGGT7zUncW5DQijNgcawcpT6reoyqkAcINsRlaOo=',
-  'sessionKeyHash': '',
-  //
-  
-  'startDate': 'March 2017',
-  'bio': 'Site co-founder. Avid traveller. Blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah.',
-  'favouritePlaces': [
-    'London',
-    'Paris',
-    'St&nbsp;Andrews',
-    'Melbourne',
-    'Cupar',
-    'Edinburgh',
-    'New&nbsp;York',
-    'Lilongwe',
-    'Dedza',
-    'Prague'
-  ],
-  'posts': [
-    {
-      'postId': 'suggest_dinner_restaurant_for_first_night_in_london',
-      'commentId': 5,
-      'postContent': 'We had a great meal at Rules last month. Although it is quite British and proper, we were dressed smart casual and were not out of place. Do...'
-    }, {
-      'postId': 'another_forum',
-      'commentId': 17,
-      'postContent': "I'd probably go for the thing"
-    }, {
-      'postId': 'asked_a_question',
-      'commentId': 0,
-      'postContent': "I'm going to Santa Cruz in Tenerife this Easter. Does anyone know the best way to climb to the top of the island from there?"
+function passwordCheck( userId, password ) {
+  $.ajax({
+    type: "GET",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'text/plain',
+    },
+    dataType: "json",
+    url: "/login/" + userId,
+    success:
+        function (data){
+          displayInfo(data);
+          console.log(JSON.stringify(data));
+        },
+    error: function () {
+      $('#output').html('Wrong password or username');
     }
-  ]
+  });
+
+  return (hash(password) == testUser.passwordHash);
+
 }
