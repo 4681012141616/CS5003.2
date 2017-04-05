aaa6e648772858ad445f38b539d4c937510d91de
YiZhang1994 <yizhang@8afbfde6.st-andrews.ac.uk>
2017-04-05 23:42:56 +0100

    convert city names

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -87,7 +87,7 @@ $(document).ready(function(){
         }
         $destinationInfo.children('h3, p').remove();
         for (var i in data.cities) {
-            $destinationInfo.prepend('<p>' + data.cities[i] + '</p>');
+            $destinationInfo.prepend('<p>' + initialUppercase(data.cities[i]) + '</p>');
         }
         $destinationInfo.prepend('<h3>' + data.fullName + '</h3>');
         //click on the city name
